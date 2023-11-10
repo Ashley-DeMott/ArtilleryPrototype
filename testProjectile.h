d1e70a67c57179c83e37e75a5d949eb0a8eed607
Galaticash <ash9280@gmail.com>
2023-11-10 13:06:24 -0700

    Setup tests for Projectile

--- testProjectile.h
+++ testProjectile.h
@@ -21,10 +21,111 @@ class TestProjectile
 public:
 	void run()
 	{
-		assert(true);
+		// Setup - Assume ground is flat, start and end at 0.0
+		testShootAngle0();
+		testShootAngle15();
+		testShootAngle30();
+		testShootAngle45();
+		testShootAngle85();
+		testShootAngleNeg15();
+		testShootAngleNeg30();
+		testShootAngleNeg45();
+		testShootAngleNeg85();
 	}
 
 private:
-	// Add Projectile
+	// Setup method (see setupGround in cpp)
+	Ground setupGround() {
+		return Ground();
+	}
+	
+	void testShootAngle0() {
+		// Setup
+		
+		// Exercise
+		
+		// Verify
+		
+		// Teardown
+	}
 
+	void testShootAngleNeg15() {
+		// Setup
+
+		// Exercise
+
+		// Verify
+
+		// Teardown
+	}
+
+	void testShootAngle15() {
+		// Setup
+
+		// Exercise
+
+		// Verify
+
+		// Teardown	
+	}
+
+	void testShootAngleNeg30() {
+		// Setup
+
+		// Exercise
+
+		// Verify
+
+		// Teardown	
+	}
+
+	void testShootAngle30() {
+		// Setup
+
+		// Exercise
+
+		// Verify
+
+		// Teardown	
+	}
+
+	void testShootAngleNeg45() {
+		// Setup
+
+		// Exercise
+
+		// Verify
+
+		// Teardown
+	}
+
+	void testShootAngle45() {
+		// Setup
+
+		// Exercise
+
+		// Verify
+
+		// Teardown	
+	}
+
+	void testShootAngleNeg85() {
+		// Setup
+
+		// Exercise
+
+		// Verify
+
+		// Teardown
+	}
+
+	void testShootAngle85() {
+		// Setup
+
+		// Exercise
+
+		// Verify
+
+		// Teardown	
+	}
 };
